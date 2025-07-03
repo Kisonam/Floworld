2df5d533485b8f600170da9a954f655178f0351e
Kisonam <kisonamplay@gmail.com>
2025-07-03 21:21:58 +0200

    Refactor ProductsController to use IProductRepository for data access and implement product seeding from JSON files

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,34 +1,38 @@
 using System;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class ProductsController(StoreContext _context) : BaseController
+public class ProductsController(IProductRepository repo) : BaseController
 {
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
     {
-        return await _context.Products.ToListAsync();
+        return Ok(await repo.GetProductsAsync());
+
     }
     [HttpGet("{id:int}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        var product = await _context.Products.FindAsync(id);
-        if (product == null) return NotFound();
+        var product = await repo.GetProductByIdAsync(id);
+        if (product == null)
+            return NotFound("Product not found with the given ID.");
         return product;
     }
 
     [HttpPost]
     public async Task<ActionResult<Product>> CreateProduct(Product product)
     {
-        _context.Products.Add(product);
-        await _context.SaveChangesAsync();
-
-        return product;
-        // return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        repo.AddProduct(product);
+        if (await repo.SaveChangesAsync())
+        {
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+        return BadRequest("Failed to create product.");
     }
 
     [HttpPut("{id:int}")]
@@ -37,10 +41,13 @@ public class ProductsController(StoreContext _context) : BaseController
         if (id != product.Id || !ProductExists(id))
             return BadRequest("Cannot update product with mismatched ID or non-existing product.");
 
-        _context.Entry(product).State = EntityState.Modified;
-        await _context.SaveChangesAsync();
+        repo.UpdateProduct(product);
+        if (await repo.SaveChangesAsync())
+        {
+            return NoContent();
+        }
 
-        return NoContent();
+        return BadRequest("Failed to update product.");
     }
 
     [HttpDelete("{id:int}")]
@@ -48,15 +55,19 @@ public class ProductsController(StoreContext _context) : BaseController
     {
         if (!ProductExists(id))
             return NotFound("Cannot delete product with non-existing ID.");
-        var product = await _context.Products.FindAsync(id);
+
+        var product = await repo.GetProductByIdAsync(id);
+        
         if (product == null) return NotFound();
-        _context.Products.Remove(product);
-        await _context.SaveChangesAsync();
+        repo.DeleteProduct(product);
+
+        if (!await repo.SaveChangesAsync())
+            return BadRequest("Failed to delete product.");
         return NoContent();
     }
 
     private bool ProductExists(int id)
     {
-        return _context.Products.Any(e => e.Id == id);
+        return repo.ProductExists(id);
     }
 }
